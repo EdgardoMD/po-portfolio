6550408b4076ebcff04ef64d2d4df6b8c9ae77d5
EdgardoMD <60943753+EdgardoMD@users.noreply.github.com>
2026-03-18 13:06:13 -0300

    Create basic models, services, directive and components

---
+++ src/app/sections/differentiators/differentiators.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-differentiators',
+  imports: [],
+  templateUrl: './differentiators.component.html',
+  styleUrl: './differentiators.component.scss'
+})
+export class DifferentiatorsComponent {
+
+}
---
+++ src/app/shared/components/tag-list/tag-list.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tag-list',
+  imports: [],
+  template: `
+    <p>
+      tag-list works!
+    </p>
+  `,
+  styles: ``
+})
+export class TagListComponent {
+
+}
